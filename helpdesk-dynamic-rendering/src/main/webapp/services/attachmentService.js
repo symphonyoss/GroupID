91174031bc29e05621ac7fc3a0ce2e2b191b27d8
Cassiano Repache <crepache@daitangroup.com>
2017-12-12 16:14:39 -0200

    Handle errors

--- helpdesk-dynamic-rendering/src/main/webapp/services/attachmentService.js
+++ helpdesk-dynamic-rendering/src/main/webapp/services/attachmentService.js
@@ -1,10 +1,7 @@
 import { approveAttachment, denyAttachment, searchAttachment } from '../api/apiCalls';
 import errorTypes from '../utils/errorTypes';
 import componentTypes from '../utils/componentTypes';
-<<<<<<< HEAD
 import messageByCode from '../errorMessages/messageByCode';
-=======
->>>>>>> 5af3fd0b5be79f1a0abf5a1914c52c88d380189d
 import messages from '../errorMessages/messages';
 
 export default class AttachmentService {
@@ -29,17 +26,13 @@ export default class AttachmentService {
     const errorMessageService = SYMPHONY.services.subscribe('error-banner');
     let errorCode;
     return denyAttachment(message)
-      .catch((error) => {
-        errorCode = parseInt(error.message, 10);
-<<<<<<< HEAD
-        const messageText = error.message ? messageByCode[errorCode]
-          : messages.GENERIC_ERROR;
-=======
-        const messageText = error.message ? messageByCode[errorCode] : messages.GENERIC_ERROR;
->>>>>>> 5af3fd0b5be79f1a0abf5a1914c52c88d380189d
-        errorMessageService.setChatBanner(message.streamId, componentTypes.CHAT,
-          messageText, errorTypes.ERROR);
-      });
+    .catch((error) => {
+      errorCode = parseInt(error.message, 10);
+      const messageText = error.message ? messageByCode[errorCode]
+        : messages.GENERIC_ERROR;
+      errorMessageService.setChatBanner(message.streamId, componentTypes.CHAT,
+        messageText, errorTypes.ERROR);
+    });
   }
 
   search(attachmentId) {
