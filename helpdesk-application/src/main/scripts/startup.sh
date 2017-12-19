adbdbce72adb7651842bf4515154b15f4cf1d6a4
Robson Vinícius Vieira Sanchez <rsanchez@symphony.com>
2017-12-19 09:58:32 -0200

    APP-1513 Upgrade install script

--- helpdesk-application/src/main/scripts/startup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-JRE_HOME=/usr/java/jdk1.8.0_73/jre
-
-$JRE_HOME/bin/java -jar /opt/symphony/helpdesk-renderer/helpdesk-renderer.jar \
-    --logging.file=/data/symphony/helpdesk-renderer/logs/helpdesk-renderer.log
--- helpdesk-distribution/src/main/scripts/install.sh
+++ helpdesk-distribution/src/main/scripts/install.sh
@@ -6,6 +6,31 @@ if [ `whoami` != 'root' ]
     exit
 fi
 
+if [[ $# -lt 2 ]]
+  then
+    echo "Usage: install.sh <MONGO_HOST> <JAVA_HOME>"
+    exit
+fi
+
+MONGO_HOST=$1
+JAVA_HOME=$2
+
+if [[ -f $JAVA_HOME/bin/java ]]
+  then
+    JAVA_PATH=$JAVA_HOME/bin/java
+elif [[ -f $JAVA_HOME/jre/bin/java ]]
+  then
+    JAVA_PATH=$JAVA_HOME/jre/bin/java
+else
+  if [[ -d $JAVA_HOME ]]
+  then
+    echo "[ERROR] Cannot find JRE at file path '$JAVA_HOME/bin/java'"
+  else
+    echo "[ERROR] Missing JRE location path"
+  fi
+  exit 1
+fi
+
 echo "Create directories"
 mkdir -p /opt/symphony/helpdesk
 mkdir -p /data/symphony/helpdesk/bin
@@ -30,9 +55,15 @@ cp -f helpdesk-bot/helpdesk-bot.jar /opt/symphony/helpdesk-bot
 cp -f helpdesk-renderer/helpdesk-renderer.jar /opt/symphony/helpdesk-renderer
 
 echo "Copy startup scripts"
-cp -f helpdesk-api/startup.sh /data/symphony/helpdesk/bin
-cp -f helpdesk-bot/startup.sh /data/symphony/helpdesk-bot/bin
-cp -f helpdesk-renderer/startup.sh /data/symphony/helpdesk-renderer/bin
+cp -f helpdesk-api/startup.sh.template /data/symphony/helpdesk/bin/startup.sh
+sed -i "s|{{ mongo_host }}|$MONGO_HOST|g" /data/symphony/helpdesk/bin/startup.sh
+sed -i "s|{{ java_path }}|$JAVA_PATH|g" /data/symphony/helpdesk/bin/startup.sh
+
+cp -f helpdesk-bot/startup.sh.template /data/symphony/helpdesk-bot/bin/startup.sh
+sed -i "s|{{ java_path }}|$JAVA_PATH|g" /data/symphony/helpdesk-bot/bin/startup.sh
+
+cp -f helpdesk-renderer/startup.sh.template /data/symphony/helpdesk-renderer/bin/startup.sh
+sed -i "s|{{ java_path }}|$JAVA_PATH|g" /data/symphony/helpdesk-renderer/bin/startup.sh
 
 yamlfile=/data/symphony/helpdesk-bot/application.yaml
 if [ -f "$yamlfile" ]
@@ -48,11 +79,11 @@ cp -f helpdesk-api/helpdesk-api.service /etc/systemd/system
 cp -f helpdesk-bot/helpdesk-bot.service /etc/systemd/system
 cp -f helpdesk-renderer/helpdesk-renderer.service /etc/systemd/system
 
+systemctl daemon-reload
+
 sleep 5
 
 echo "Starting services"
-systemctl daemon-reload
-
 service helpdesk-api start
 sleep 3
 service helpdesk-renderer start
