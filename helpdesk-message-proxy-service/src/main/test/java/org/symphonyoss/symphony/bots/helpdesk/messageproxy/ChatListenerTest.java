dc90ae228fdd2815eab11fc5a8fbb806dac45900
Robson Vinícius Vieira Sanchez <rsanchez@symphony.com>
2018-01-04 17:32:48 -0200

    APP-1633 Sending an attachment without text does not trigger maker/checker messages

--- helpdesk-message-proxy-service/src/main/test/java/org/symphonyoss/symphony/bots/helpdesk/messageproxy/ChatListenerTest.java
+++ helpdesk-message-proxy-service/src/main/test/java/org/symphonyoss/symphony/bots/helpdesk/messageproxy/ChatListenerTest.java
@@ -9,7 +9,11 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.symphonyoss.symphony.bots.ai.HelpDeskAi;
+import org.symphonyoss.symphony.clients.model.SymAttachmentInfo;
 import org.symphonyoss.symphony.clients.model.SymMessage;
+
+import java.io.IOException;
+import java.util.Collections;
 
 /**
  * Created by rsanchez on 18/12/17.
@@ -26,14 +30,35 @@ public class ChatListenerTest {
   private HelpDeskAi helpDeskAi;
 
   @Test
-  public void testEmptyMessage() {
+  public void testWithoutContent() {
     SymMessage message = new SymMessage();
 
     ChatListener listener = new ChatListener(ticketManagerService, helpDeskAi);
+    listener.ready();
+
     listener.onMessage(message);
 
     verify(ticketManagerService, never()).messageReceived(message);
     verify(helpDeskAi, never()).onMessage(message);
+
+    message.setAttachments(Collections.emptyList());
+
+    verify(ticketManagerService, never()).messageReceived(message);
+    verify(helpDeskAi, never()).onMessage(message);
+  }
+
+  @Test
+  public void testEmptyMessage() throws IOException {
+    SymMessage message = new SymMessage();
+    message.setAttachments(Collections.singletonList(new SymAttachmentInfo()));
+
+    ChatListener listener = new ChatListener(ticketManagerService, helpDeskAi);
+    listener.ready();
+
+    listener.onMessage(message);
+
+    verify(ticketManagerService, times(1)).messageReceived(message);
+    verify(helpDeskAi, times(1)).onMessage(message);
   }
 
   @Test
