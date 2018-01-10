84742d59de85593c7bd328ed0fd44a3f481a90f6
Cassiano Repache <crepache@daitangroup.com>
2018-01-10 15:43:12 -0200

    Revert changes

--- helpdesk-dynamic-rendering/src/main/webapp/js/controller.js
+++ helpdesk-dynamic-rendering/src/main/webapp/js/controller.js
@@ -4,7 +4,7 @@ import AttachmentEnricher from '../enrichers/attachmentEnricher';
 
 const claimTicketEnricher = new ClaimTicketEnricher();
 const attachmentEnricher = new AttachmentEnricher();
-const appId = 'helpdeskCassiano';
+const appId = 'helpdesk';
 const controllerName = 'helpdesk:controller';
 
 const registerApp = () => {
