916ef91214cce842c3faf4be46d7297a68172e0d
Cassiano Repache <crepache@daitangroup.com>
2018-01-24 15:21:53 -0200

    Fix locations

--- build.sh
+++ build.sh
@@ -6,13 +6,13 @@ echo "Building group-id + helpdesk"
 mvn clean install
   
 echo "Building Helpdesk-api docker image"
-echo "docker build -f helpdesk_Api/docker/Dockerfile -t helpdesk_api ."
-docker build -f helpdesk_api/docker/Dockerfile -t helpdesk-api  .
+echo "docker build -f helpdesk-service/helpdesk-service-api/docker/Dockerfile -t helpdesk-api  ."
+docker build -f helpdesk-service/helpdesk-service-api/docker/Dockerfile -t helpdesk-api  .
  
 echo "Building Helpdesk-bot docker image"
-echo "docker build -f helpdesk_Bot/docker/Dockerfile -t helpdesk_bot ."
-docker build -f helpdesk_Bot/docker/Dockerfile -t helpdesk_bot  .
+echo "docker build -f helpdesk-bot/docker/Dockerfile -t helpdesk_bot ."
+docker build -f helpdesk-bot/docker/Dockerfile -t helpdesk-bot  .
  
 echo "Building Helpdesk-renderer docker image"
-echo "docker build -f helpdesk_Renderer/docker/Dockerfile -t helpdesk-renderer ."
-docker build -f helpdesk_Renderer/docker/Dockerfile -t helpdesk-renderer  .
+echo "docker build -f helpdesk-application/docker/Dockerfile -t helpdesk-application ."
+docker build -f helpdesk-application/docker/Dockerfile -t helpdesk-application  .
