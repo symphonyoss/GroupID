27cd7da54328198ff6c0e3b879fad5a4c7296554
Luana Pimentel <lpimentel@daitangroup.com>
2018-02-05 15:31:21 -0200

    APP-1541 - Added new Unit Tests

--- helpdesk-bot/src/test/java/org/symphonyoss/symphony/bots/helpdesk/bot/listener/HelpDeskRoomEventListenerTest.java
+++ helpdesk-bot/src/test/java/org/symphonyoss/symphony/bots/helpdesk/bot/listener/HelpDeskRoomEventListenerTest.java
@@ -1,6 +1,9 @@
 package org.symphonyoss.symphony.bots.helpdesk.bot.listener;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
@@ -14,16 +17,20 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.symphonyoss.client.SymphonyClient;
 import org.symphonyoss.client.events.SymUserJoinedRoom;
+import org.symphonyoss.client.events.SymUserLeftRoom;
 import org.symphonyoss.client.exceptions.MessagesException;
 import org.symphonyoss.client.services.MessageService;
 import org.symphonyoss.symphony.bots.helpdesk.bot.config.HelpDeskBotConfig;
-import org.symphonyoss.symphony.bots.helpdesk.bot.listener.HelpDeskRoomEventListener;
 import org.symphonyoss.symphony.bots.helpdesk.messageproxy.config.InstructionalMessageConfig;
 import org.symphonyoss.symphony.bots.helpdesk.messageproxy.service.TicketService;
+import org.symphonyoss.symphony.bots.helpdesk.service.model.Ticket;
+import org.symphonyoss.symphony.bots.helpdesk.service.model.UserInfo;
 import org.symphonyoss.symphony.bots.helpdesk.service.ticket.client.TicketClient;
 import org.symphonyoss.symphony.clients.model.SymMessage;
 import org.symphonyoss.symphony.clients.model.SymStream;
 import org.symphonyoss.symphony.clients.model.SymUser;
+
+import java.util.Arrays;
 
 /**
  * Created by rsanchez on 14/12/17.
@@ -35,7 +42,11 @@ public class HelpDeskRoomEventListenerTest {
 
   private static final String MOCK_STREAM = "Yc-my4qYo4-ZoQyR6C16o3___q_zYfhtWB";
 
+  private static final String MOCK_STREAM2 = "anyStream";
+
   private static final Long MOCK_BOT_USER = 654321L;
+
+  private static final Long MOCK_ANY_USER = 032121L;
 
   private static final String MOCK_BOT_STREAM = "Zs-nx3pQh3-XyKlT5B15m3___p_zHfetdA";
 
@@ -75,12 +86,14 @@ public class HelpDeskRoomEventListenerTest {
 
     doReturn(symUser).when(symphonyClient).getLocalUser();
 
-    this.listener = new HelpDeskRoomEventListener(runawayAgentMessage, symphonyClient, ticketClient, config, messageConfig, ticketService);
+    this.listener =
+        new HelpDeskRoomEventListener(runawayAgentMessage, symphonyClient, ticketClient, config,
+            messageConfig, ticketService);
   }
 
   @Test
   public void testJoinedRoomIsNotABotUser() throws MessagesException {
-    SymUserJoinedRoom joinedRoom = mockEvent(MOCK_USER, MOCK_STREAM);
+    SymUserJoinedRoom joinedRoom = mockJoinEvent(MOCK_USER, MOCK_STREAM);
 
     listener.onSymUserJoinedRoom(joinedRoom);
 
@@ -89,7 +102,7 @@ public class HelpDeskRoomEventListenerTest {
 
   @Test
   public void testJoinedRoomIsAgentStream() throws MessagesException {
-    SymUserJoinedRoom joinedRoom = mockEvent(MOCK_BOT_USER, MOCK_BOT_STREAM);
+    SymUserJoinedRoom joinedRoom = mockJoinEvent(MOCK_BOT_USER, MOCK_BOT_STREAM);
 
     listener.onSymUserJoinedRoom(joinedRoom);
 
@@ -98,14 +111,54 @@ public class HelpDeskRoomEventListenerTest {
 
   @Test
   public void testJoinedRoom() throws MessagesException {
-    SymUserJoinedRoom joinedRoom = mockEvent(MOCK_BOT_USER, MOCK_STREAM);
+    SymUserJoinedRoom joinedRoom = mockJoinEvent(MOCK_BOT_USER, MOCK_STREAM);
 
     listener.onSymUserJoinedRoom(joinedRoom);
 
     verify(messageService, times(1)).sendMessage(any(SymStream.class), any(SymMessage.class));
   }
 
-  private SymUserJoinedRoom mockEvent(Long userId, String stream) {
+  @Test
+  public void testUserLeftRoomOnlyTheBotRemains() {
+    SymUserLeftRoom symUserLeftRoom = mockLeaveEvent(MOCK_ANY_USER, MOCK_STREAM);
+
+    Ticket mockTicket = new Ticket();
+    mockTicket.setAgent(new UserInfo());
+    mockTicket.setState(TicketClient.TicketStateType.UNRESOLVED.toString());
+
+    doReturn(MOCK_STREAM2).when(config).getAgentStreamId();
+    doReturn(mockTicket).when(ticketClient).getTicketByServiceStreamId(eq(MOCK_STREAM));
+
+    listener.onSymUserLeftRoom(symUserLeftRoom);
+
+    assertEquals(null, mockTicket.getAgent());
+    assertEquals(TicketClient.TicketStateType.UNSERVICED.toString(), mockTicket.getState());
+
+    verify(ticketClient, times(1)).getTicketByServiceStreamId(MOCK_STREAM);
+    verify(ticketClient, times(1)).updateTicket(eq(mockTicket));
+    verify(ticketService, times(1)).sendTicketMessageToAgentStreamId(eq(mockTicket),
+        any(SymMessage.class));
+    verify(ticketService, times(1)).sendClientMessageToServiceStreamId(anyString(),
+        any(SymMessage.class));
+  }
+
+  @Test
+  public void testBotLeftRoom() {
+    SymUser symUser = new SymUser();
+    symUser.setId(MOCK_BOT_USER);
+    symUser.setDisplayName(StringUtils.EMPTY);
+
+    SymUserLeftRoom symUserLeftRoom = mockLeaveEvent(MOCK_BOT_USER, MOCK_STREAM);
+
+    doReturn(MOCK_STREAM).when(config).getAgentStreamId();
+    doReturn(symUser).when(symphonyClient).getLocalUser();
+
+    listener.onSymUserLeftRoom(symUserLeftRoom);
+
+    verify(symphonyClient, times(1)).getLocalUser();
+  }
+
+  private SymUserJoinedRoom mockJoinEvent(Long userId, String stream) {
     SymUser symUser = new SymUser();
     symUser.setId(userId);
     symUser.setDisplayName(StringUtils.EMPTY);
@@ -119,6 +172,23 @@ public class HelpDeskRoomEventListenerTest {
     symUserJoinedRoom.setStream(symStream);
 
     return symUserJoinedRoom;
+  }
+
+  private SymUserLeftRoom mockLeaveEvent(Long userId, String stream) {
+    SymUser symUser = new SymUser();
+    symUser.setId(userId);
+    symUser.setDisplayName(StringUtils.EMPTY);
+
+    SymStream symStream = new SymStream();
+    symStream.setRoomName(StringUtils.EMPTY);
+    symStream.setStreamId(stream);
+    symStream.setMembers(Arrays.asList(symUser));
+
+    SymUserLeftRoom symUserLeftRoom = new SymUserLeftRoom();
+    symUserLeftRoom.setAffectedUser(symUser);
+    symUserLeftRoom.setStream(symStream);
+
+    return symUserLeftRoom;
   }
 
 }
