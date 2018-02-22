751c1a8a1dd9215dd081f4b075aa19e61f704c17
Robson Vinícius Vieira Sanchez <rsanchez@symphony.com>
2018-02-22 15:20:51 -0300

    APP-1623 HelpDesk Service - Setup JBehave with Spring to run integration tests in the Maven lifecycle

--- helpdesk-service/helpdesk-service-server/src/test/java/org/symphonyoss/symphony/bots/helpdesk/service/HelpDeskServiceStories.java
+++ helpdesk-service/helpdesk-service-server/src/test/java/org/symphonyoss/symphony/bots/helpdesk/service/HelpDeskServiceStories.java
@@ -1,35 +1,64 @@
 package org.symphonyoss.symphony.bots.helpdesk.service;
 
-import org.junit.Test;
+import org.jbehave.core.configuration.MostUsefulConfiguration;
+import org.jbehave.core.failures.FailingUponPendingStep;
+import org.jbehave.core.io.CodeLocations;
+import org.jbehave.core.io.LoadFromClasspath;
+import org.jbehave.core.io.StoryFinder;
+import org.jbehave.core.junit.JUnitStories;
+import org.jbehave.core.reporters.CrossReference;
+import org.jbehave.core.reporters.Format;
+import org.jbehave.core.reporters.PrintStreamStepdocReporter;
+import org.jbehave.core.reporters.StoryReporterBuilder;
+import org.jbehave.core.steps.InjectableStepsFactory;
+import org.jbehave.core.steps.SilentStepMonitor;
+import org.jbehave.core.steps.spring.SpringStepsFactory;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.ResponseEntity;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.symphonyoss.symphony.bots.helpdesk.service.model.TicketSearchResponse;
+
+import java.util.List;
 
 /**
  * Created by rsanchez on 21/02/18.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = { HelpDeskServiceInit.class })
-public class HelpDeskServiceStories {
-
-  private static final Logger LOGGER = LoggerFactory.getLogger(HelpDeskServiceStories.class);
+public class HelpDeskServiceStories extends JUnitStories {
 
   @Autowired
-  private TestRestTemplate template;
+  private ApplicationContext applicationContext;
 
-  @Test
-  public void dummyTest() {
-    ResponseEntity<TicketSearchResponse> entity =
-        template.getForEntity("/v1/ticket/search=testgroup", TicketSearchResponse.class);
+  public HelpDeskServiceStories() {
+    initJBehaveConfiguration();
+  }
 
-    TicketSearchResponse tickets = entity.getBody();
-    LOGGER.info("Ticket search: " + tickets);
+  private void initJBehaveConfiguration() {
+    Class<?> thisClass = this.getClass();
+    useConfiguration(new MostUsefulConfiguration()
+        .useStoryLoader(new LoadFromClasspath(thisClass.getClassLoader()))
+        .usePendingStepStrategy(new FailingUponPendingStep())
+        .useStepdocReporter(new PrintStreamStepdocReporter())
+        .useStoryReporterBuilder(new StoryReporterBuilder()
+            .withCodeLocation(CodeLocations.codeLocationFromClass(thisClass))
+            .withDefaultFormats()
+            .withFormats(Format.CONSOLE, Format.TXT, Format.HTML, Format.XML, Format.STATS)
+            .withCrossReference(new CrossReference())
+            .withFailureTrace(true))
+        .useStepMonitor(new SilentStepMonitor()));
+  }
+
+  @Override
+  public InjectableStepsFactory stepsFactory() {
+    return new SpringStepsFactory(configuration(), applicationContext);
+  }
+
+  @Override
+  protected List<String> storyPaths() {
+    return new StoryFinder().findPaths(CodeLocations.codeLocationFromClass(this.getClass()),
+        "**/*.story", "**/excluded*.story");
   }
 
 }
