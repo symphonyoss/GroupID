4f5bc19293400da0def7dd3b6ed195ab0e165576
Alexandre Ribeiro da Silva <alexandre.ribeiro@symphony.com>
2018-03-19 17:25:48 -0300

    PLAT-4839: Unit test from helpdesk-service-client module

---
+++ helpdesk-ai/src/test/java/org/symphonyoss/symphony/bots/ai/impl/AiResponderImplTest.java
@@ -0,0 +1,33 @@
+package org.symphonyoss.symphony.bots.ai.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AiResponderImplTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @Test
+  public void respond() {
+  }
+
+  @Test
+  public void addResponse() {
+  }
+
+  @Test
+  public void respondWithUseMenu() {
+  }
+
+  @Test
+  public void respondWithSuggestion() {
+  }
+
+  @Test
+  public void getBestCommand() {
+  }
+}
---
+++ helpdesk-service/helpdesk-service-client/src/test/org/symphonyoss/symphony/bots/helpdesk/service/makerchecker/client/MakercheckerClientTest.java
@@ -0,0 +1,93 @@
+package org.symphonyoss.symphony.bots.helpdesk.service.makerchecker.client;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doReturn;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.symphonyoss.symphony.bots.helpdesk.service.HelpDeskApiException;
+import org.symphonyoss.symphony.bots.helpdesk.service.api.MakercheckerApi;
+import org.symphonyoss.symphony.bots.helpdesk.service.client.ApiClient;
+import org.symphonyoss.symphony.bots.helpdesk.service.client.ApiException;
+import org.symphonyoss.symphony.bots.helpdesk.service.client.Configuration;
+import org.symphonyoss.symphony.bots.helpdesk.service.model.Makerchecker;
+import org.symphonyoss.symphony.bots.helpdesk.service.model.UserInfo;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MakercheckerClientTest {
+
+  private static final String MOCK_SERVICE_STREAM_ID = "MOCK_SERVICE_STREAM_ID";
+  private static final String MOCK_MAKERCHECKER_ID = "MOCK_MAKERCHECKER_ID";
+  private static final Long MOCK_MAKER_ID = 123L;
+  private static final String DISPLAY_NAME = "DISPLAY_NAME";
+  private static final Long MOCK_USER_ID = 456L;
+
+  private final String groupId = "GROUP_ID";
+
+  private final String TICKET_SERVICE_URL = "https://localhost/helpdesk-service";
+
+  @Mock
+  private ApiClient apiClient;
+
+  private MakercheckerClient makercheckerClient;
+
+  @Before
+  public void setUp() throws Exception {
+    Configuration.setDefaultApiClient(apiClient);
+    makercheckerClient = new MakercheckerClient(groupId, TICKET_SERVICE_URL);
+  }
+
+  @Test
+  public void getMakerchecker() throws ApiException {
+    doReturn(makercheckerMock()).when(apiClient)
+        .invokeAPI(any(), eq("GET"), any(), any(), any(), any(), any(), any(), any(), any());
+
+    doReturn(MOCK_MAKERCHECKER_ID).when(apiClient).escapeString(any());
+
+    Makerchecker makerchecker = makercheckerClient.getMakerchecker(groupId);
+
+    assertNotNull(makerchecker);
+    assertEquals(makercheckerMock(), makerchecker);
+
+  }
+
+  @Test(expected = HelpDeskApiException.class)
+  public void getMakercheckerWithNullId() throws ApiException {
+
+    Makerchecker makerchecker = makercheckerClient.getMakerchecker(null);
+
+  }
+
+  @Test
+  public void createMakerchecker() {
+  }
+
+  @Test
+  public void updateMakerchecker() {
+  }
+
+  private Makerchecker makercheckerMock() {
+    Makerchecker makerchecker = new Makerchecker();
+    makerchecker.setChecker(userInfoMock());
+    makerchecker.setState(MakercheckerClient.AttachmentStateType.OPENED.getState());
+    makerchecker.setStreamId(MOCK_SERVICE_STREAM_ID);
+    makerchecker.setId(MOCK_MAKERCHECKER_ID);
+    makerchecker.setMakerId(MOCK_MAKER_ID);
+
+    return makerchecker;
+  }
+
+  private UserInfo userInfoMock() {
+    UserInfo userInfo = new UserInfo();
+    userInfo.displayName(DISPLAY_NAME);
+    userInfo.setUserId(MOCK_USER_ID);
+
+    return userInfo;
+  }
+}
