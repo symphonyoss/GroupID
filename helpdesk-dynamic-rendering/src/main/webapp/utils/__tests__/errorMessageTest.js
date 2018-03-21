1c00d94bebf7a1e07578a3b48305bf25cedb044e
Lucas Moura <lucasmour4@gmail.com>
2018-03-21 13:19:36 -0300

    PLAT-4831 Utils 100% covered, and added support to reconfigure jsdom

--- helpdesk-dynamic-rendering/src/main/webapp/utils/__tests__/errorMessageTest.js
+++ helpdesk-dynamic-rendering/src/main/webapp/utils/__tests__/errorMessageTest.js
@@ -20,20 +20,20 @@ const expectedErrorMessage = {
 };
 
 describe('Error Message', () => {
-    beforeEach(() => {
-        error.mockClear();
-    });
-    it('Should render error message', () => {
-        const errorMessage = renderErrorMessage(mockEntity, mockMessageError, mockEnricherServiceName);
+    error.mockClear();
+    const errorMessage = renderErrorMessage(mockEntity, mockMessageError, mockEnricherServiceName);
 
+    it('Should call \'actionFactory\' function with correct parameters', () => {
         expect(actionFactory.mock.calls.length).toBe(1);
         expect(actionFactory.mock.calls[0][0]).toEqual([]);
         expect(actionFactory.mock.calls[0][1]).toEqual(mockEnricherServiceName);
         expect(actionFactory.mock.calls[0][2]).toEqual(mockEntity);
-
+    });
+    it('Should call \'error\' template with correct parameters', () => {
         expect(error.mock.calls.length).toBe(1);
         expect(error.mock.calls[0][0].hasOwnProperty('message')).toBe(true);
-
+    });
+    it('Should render error message', () => {
         expect(errorMessage).toEqual(expectedErrorMessage);
     });
 });
