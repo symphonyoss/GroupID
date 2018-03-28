7d24c91dde4cc97a11af63c756ec79a27255fbac
Cassiano Repache <crepache@daitangroup.com>
2018-03-28 14:22:50 -0300

    Reorganize init helpdesk bot

--- helpdesk-dynamic-rendering/src/main/webapp/js/controller.js
+++ helpdesk-dynamic-rendering/src/main/webapp/js/controller.js
@@ -4,6 +4,7 @@ import ClaimTicketEnricher from '../enrichers/claimTicketEnricher';
 import ActionClaimTicketEnricher from '../enrichers/actionClaimTicketEnricher';
 import AttachmentEnricher from '../enrichers/attachmentEnricher';
 import ActionAttachmentEnricher from '../enrichers/actionAttachmentEnricher';
+import { bootstrapService } from 'symphony-app-authentication-fe';
 
 const claimTicketEnricher = new ClaimTicketEnricher();
 const attachmentEnricher = new AttachmentEnricher();
@@ -14,23 +15,39 @@ const appName = getParameterByName('id');
 const appId = appName || 'helpdesk';
 const controllerName = `${appId}:controller`;
 
-const registerApp = () => {
+const initEnrichers = () => {
   claimTicketEnricher.init();
   attachmentEnricher.init();
   actionClaimTicketEnricher.init();
   actionAttachmentEnricher.init();
-
-  return SYMPHONY.application.register(appId, ['ui', 'entity', 'extended-user-service', 'error-banner'], [controllerName, claimTicketEnricher.name, attachmentEnricher.name, actionClaimTicketEnricher.name, actionAttachmentEnricher.name]);
 };
 
-SYMPHONY.services.register(controllerName);
+const registerEnrichers = () => {
+  claimTicketEnricher.register();
+  attachmentEnricher.register();
+  actionClaimTicketEnricher.register();
+  actionAttachmentEnricher.register();
+};
 
-SYMPHONY.remote.hello()
-  .then(registerApp)
-  .then(() => {
-    claimTicketEnricher.register();
-    attachmentEnricher.register();
-    actionClaimTicketEnricher.register();
-    actionAttachmentEnricher.register();
-  })
-  .fail(() => console.error('Fail to register helpdesk dynamic rendering'));
+const initAuthentication = () => {
+  let config;
+  config.appId = appId;
+  config.dependencies = ['ui', 'entity', 'extended-user-service', 'error-banner'];
+  config.exportedDependencies = [controllerName, claimTicketEnricher.name,
+    attachmentEnricher.name, actionClaimTicketEnricher.name, actionAttachmentEnricher.name];
+  config.baseAuthenticationUrl = 'localhost:8080';
+
+  bootstrapService.initApp(config);
+};
+
+const initApp = () => {
+  SYMPHONY.services.register(controllerName);
+
+  initEnrichers();
+
+  initAuthentication();
+
+  registerEnrichers();
+};
+
+initApp();
