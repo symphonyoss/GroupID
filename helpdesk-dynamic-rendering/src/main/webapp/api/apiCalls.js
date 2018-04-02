c6e9f4c0d043196ef6d29ee41812221f16f8f05a
Cassiano Repache <crepache@daitangroup.com>
2018-04-02 10:30:17 -0300

    Add JWT on header

--- helpdesk-dynamic-rendering/src/main/webapp/api/apiCalls.js
+++ helpdesk-dynamic-rendering/src/main/webapp/api/apiCalls.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import UserService from 'symphony-app-authentication-fe';
 
 const rejectPromise = (error) => {
   const response = error.response || {};
@@ -6,11 +7,17 @@ const rejectPromise = (error) => {
   return Promise.reject(new Error(status));
 };
 
+const userService = new UserService();
+
 export const claimTicket = (data) => {
   const apiUrl = `${data.entity.claimUrl}`;
+  const jwt = userService.getUserJWT();
   return axios({
     method: 'post',
     url: apiUrl,
+    headers: {
+      Authorization: `Bearer, ${jwt}`,
+    },
     params: {
       agentId: data.userId,
     },
@@ -19,9 +26,13 @@ export const claimTicket = (data) => {
 
 export const joinConversation = (data) => {
   const apiUrl = `${data.entity.joinUrl}`;
+  const jwt = userService.getUserJWT();
   return axios({
     method: 'post',
     url: apiUrl,
+    headers: {
+      Authorization: `Bearer, ${jwt}`,
+    },
     params: {
       agentId: data.userId,
     },
@@ -30,18 +41,26 @@ export const joinConversation = (data) => {
 
 export const getTicket = (ticketUrl) => {
   const apiUrl = `${ticketUrl}`;
+  const jwt = userService.getUserJWT();
   return axios({
     method: 'get',
     url: apiUrl,
+    headers: {
+      Authorization: `Bearer, ${jwt}`,
+    },
   })
   .catch(error => rejectPromise(error));
 };
 
 export const approveAttachment = (approveUrl, userId) => {
   const apiUrl = `${approveUrl}`;
+  const jwt = userService.getUserJWT();
   return axios({
     method: 'post',
     url: apiUrl,
+    headers: {
+      Authorization: `Bearer, ${jwt}`,
+    },
     params: {
       userId,
     },
@@ -50,9 +69,13 @@ export const approveAttachment = (approveUrl, userId) => {
 
 export const denyAttachment = (denyUrl, userId) => {
   const apiUrl = `${denyUrl}`;
+  const jwt = userService.getUserJWT();
   return axios({
     method: 'post',
     url: apiUrl,
+    headers: {
+      Authorization: `Bearer, ${jwt}`,
+    },
     params: {
       userId,
     },
@@ -61,8 +84,12 @@ export const denyAttachment = (denyUrl, userId) => {
 
 export const searchAttachment = (attachmentUrl) => {
   const apiUrl = `${attachmentUrl}`;
+  const jwt = userService.getUserJWT();
   return axios({
     method: 'get',
     url: apiUrl,
+    headers: {
+      Authorization: `Bearer, ${jwt}`,
+    },
   }).catch(error => rejectPromise(error));
 };
